7b4468ccabf1afd8543ddcff859b8c9406ac9a2e
Rizky208 <rizky160@gmail.com>
2022-12-26 14:58:35 +0700

    Menambahkan Toggle Button pada LP dan responsive max 800px

---
+++ index.js
@@ -0,0 +1,6 @@
+// const toggleButton = document.getElementById("toggle-button");
+// const navbarList = document.getElementById("ul-navbar");
+
+// toggleButton.addEventListener("click", () => {
+//   navbarList.classlist.toggle("active");
+// });
